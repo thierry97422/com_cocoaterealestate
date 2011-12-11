04c541b26f14c7f16d82455add387de46397a85c
Hagen Graf <hagen.graf@gmail.com>
2011-12-11 21:41:58 +0100

    Initial Commit

---
+++ admin/views/objects/view.html.php
@@ -0,0 +1,32 @@
+<?php 
+// No direct access to this file
+defined('_JEXEC') or die;
+jimport('joomla.application.component.view');
+class CocoateRealEstateViewObjects extends JView
+{
+  function display($tpl = null) 
+  {
+    // Get data from the model
+    $items = $this->get('Items');
+    $pagination = $this->get('Pagination');
+ 
+    // Assign data to the view
+    $this->items = $items;
+    $this->pagination = $pagination;
+                
+    // Set the toolbar
+    $this->addToolBar();
+ 
+    // Display the template
+    parent::display($tpl);
+  }
+        
+  protected function addToolBar() 
+  {
+    JToolBarHelper::title(JText::_('COM_COCOATEREALESTATE_MANAGER_OBJECTS'));
+    JToolBarHelper::deleteListX('', 'objects.delete');
+    JToolBarHelper::editListX('object.edit');
+    JToolBarHelper::addNewX('object.add');
+  }      
+}
+?>
